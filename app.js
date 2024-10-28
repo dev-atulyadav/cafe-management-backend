99b586902a3aca4ab071b4bf5488949df00a0a31
dev-atulyadav <atulyadav031103@gmail.com>
2024-10-28 22:33:50 +0530

    getUserByEmail & deleteUserByEmail API created

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import session from "express-session";
 import UserModel from "./models/User.model.js";
 import bcrypt from "bcryptjs";
 import DishModel from "./models/Dish.model.js";
 const app = express();
 const port = 3000;
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use((req, res, next) => {
+  console.log("Session:", req.session);
+  next();
+});
 // Connect to MongoDB database
 try {
   mongoose
     .connect("mongodb://localhost:27017/cafe-management")
     .then(() => console.log("Connected to MongoDB"));
 } catch (error) {
-  console.log(error);
+  console.error("Error connecting to MongoDB:", error);
 }
 
 app.use(
@@ -47,16 +59,26 @@ app.post("/addDish", async (req, res) => {
   }
 });
 
-app.get("/register/:email/:password", (req, res) => {
-  const { email, password } = req.params;
-  const hasedPassword = bcrypt.hashSync(req.params.password, 10);
-  const newUser = new UserModel({
-    email,
-    password: hasedPassword,
-    cart: [],
-  });
-  newUser.save();
-  console.log(newUser);
+app.post("/register", async (req, res) => {
+  const { name, email, phone, address, password } = req.body;
+  console.log(name, email, phone, address, password);
+
+  const existingUser = await UserModel.findOne({ email });
+  if (existingUser) {
+    return res.json({ status: 400, message: "User already exists" });
+  } else {
+    const hasedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new UserModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hasedPassword,
+      cart: [],
+      orders: [],
+    });
+    newUser.save();
+  }
 
   console.log(email, password);
   return res.json({ status: 201, message: "User Created!" });
@@ -65,7 +87,7 @@ app.get("/register/:email/:password", (req, res) => {
 app.get("/login/:email/:password", async (req, res) => {
   const { email, password } = req.params;
   const existingUser = await UserModel.findOne({ email });
-  console.log(existingUser);
+  console.log();
 
   if (existingUser) {
     const isValidPassword = bcrypt.compareSync(password, existingUser.password);
@@ -75,29 +97,72 @@ app.get("/login/:email/:password", async (req, res) => {
       return res.json({ status: 401, message: "Invalid password!" });
     }
   } else {
-    return res.json({ status: 404, message: "Plese login" });
+    return res.json({ status: 404, message: "Please register first!" });
   }
 });
 app.get("/addToCart/:dishId/:userEmail", async (req, res) => {
   const { dishId, userEmail } = req.params;
   const existingUser = await UserModel.findOne({ email: userEmail });
+  console.log(dishId);
+  console.log(userEmail);
+  let quantity = 1;
+
   if (existingUser) {
-    const existingCart = existingUser.cart.find(
-      (item) => item.dishId === dishId
-    );
-    if (existingCart) {
-      existingCart.quantity += 1;
+    const { cart } = existingUser;
+    if (cart.length > 0) {
+      for (const element of cart) {
+        if (element.id === dishId) {
+          console.log(cart);
+
+          element.quantity += quantity;
+          existingUser.save();
+          return res.json({ status: 200, message: "Dish added to cart!" });
+        } else {
+          const newDish = { dishId, quantity };
+          cart.push(newDish);
+          existingUser.save();
+        }
+        return res.json({ status: 200, message: "Dish added to cart!" });
+      }
     } else {
-      existingUser.cart.push({ dishId: dishId, quantity: 1 });
+      const newDish = { dishId, quantity };
+      cart.push(newDish);
+      existingUser.save();
+      return res.json({ status: 200, message: "Dish added to cart!" });
     }
-    existingUser.save();
-    return res.json({ status: 200, message: "Dish added to cart!" });
-  } else {
-    return res.json({ status: 404, message: "User not found!" });
   }
+  return res.json({ status: 404, message: "User not found!" });
 });
 
+app.get("/getUserByEmail/:email", async (req, res) => {
+  const { email } = req.params;
+  const existingUser = await UserModel.findOne({ email });
+  const user = {
+    name: existingUser.name,
+    email: existingUser.email,
+    phone: existingUser.phone,
+    address: existingUser.address,
+    cart: existingUser.cart,
+    orders: existingUser.orders,
+  };
+  if (existingUser) {
+    return res.json({
+      status: 200,
+      message: "User found!",
+      data: user,
+    });
+  }
+  return res.json({ status: 404, message: "User not found!" });
+});
+
+app.delete("/deleteUserByEmail", async (req, res) => {
+  const { email } = req.body;
+  const existingUser = await UserModel.findOneAndDelete({ email });
+  if (!existingUser) {
+    return res.json({ status: 200, message: "User deleted!" });
+  }
+  return res.json({ status: 404, message: "User not found!" });
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
- 
--- models/User.model.js
+++ models/User.model.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 export const userSchema = mongoose.Schema({
   name: String,
   email: String,
+  phone: Number,
+  address: String,
   password: String,
   cart: [],
+  orders: [],
 });
 
 export default mongoose.model("User", userSchema);
